39a390b10e4f3422dbfe9ea4b836f4125f357efe
Aditya Aggarwal <adityaaggarwal7861@gmail.com>
2021-07-29 23:55:00 +0530

    Add solution for problem `B - Cobb` Link: https://codeforces.com/contest/1554/problem/B Tags: bitmasks, brute force, greedy, math, *1700 Ref: https://codeforces.com/contest/1554/submission/124193263

--- codeforces/1554/B.java
+++ codeforces/1554/B.java
@@ -34,7 +34,7 @@ public class Main {
                 for (int i=0;i<n;i++) list.add(r.nl());
 
                 long ans = Integer.MIN_VALUE;
-                for (int i=Math.max(n-200,1);i<=n;i++){
+                for (int i=Math.max(n-500,1);i<=n;i++){
                     for (int j=i+1;j<=n;j++){
                         ans = Math.max(ans, (long) i *j-k*(list.get(i)|list.get(j)));
                     }
